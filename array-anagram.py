b83d6c741f3964a6ebbb7b60d8fc422a94a4ab89
Efe Mayoğlu <efemayoglu@hotmail.com>
2021-03-11 21:59:38 +0300

    Create array-anagram.py

---
+++ array-anagram.py
@@ -0,0 +1,29 @@
+def anagram(s1,s2):
+    a = {}
+    for i in s1:
+        if i not in a:
+            if i != ' ':
+                a[i] = 1
+        else:
+            a[i] += 1
+            
+    for i in s2:
+        if i != ' ':
+            if i in a:
+                if a[i] > 0:
+                    a[i] = a[i] -1
+                else:
+                    return False
+            else:
+                return False
+
+    for i in a.keys():
+        if a[i] != 0:
+            return False
+
+    return True
+            
+            
+print(anagram('dog','god'))
+print(anagram('clint eastwood','old west action'))
+print(anagram('public relations','crap built on lies'))
