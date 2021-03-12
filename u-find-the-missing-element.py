f560510901db3e9de4b9c2676edb7538e438ce15
efemayoglu <efemayoglu@gmail.com>
2021-03-12 11:49:23 +0300

    naive solution  is way better

--- u-find-the-missing-element.py
+++ u-find-the-missing-element.py
@@ -1,38 +1,44 @@
-def finder(arr1, arr2):
-    arr1 = list(sorted(arr1))
-    arr2 = list(sorted(arr2))
+# def finder(arr1, arr2):
+#     arr1.sort()
+#     arr2.sort()
 
-    for i in range(len(arr1)):
-        if arr1[i] != arr2[i]:
-            return arr1[i]
+#     for i in range(len(arr1)):
+#         if arr1[i] != arr2[i]:
+#             return arr1[i]
 
-    return -1
-
+#     return -1
 
 
 # def finder(arr1, arr2):
-#     a = {}
+#     arr1.sort()
+#     arr2.sort()
 
-#     for i in arr1:
-#         if i not in a:
-#             a[i] = 1
-#         else:
-#             a[i] = a[i] + 1
-    
-#     for i in arr2:
-#         if i not in a:
-#             return i
-#         else:
-#             a[i] = a[i] -1
+#     for num1, num2 in zip(arr1, arr2):
+#         if num1 != num2:
+#             return num1
+
+#     return arr2[-1]
 
 
-#     print(a)
-#     for i in a.keys():
-#         print(f'i = {i}, a[{i}]= {a[i]}')
-#         if a[i] !=0:
-#             return i
-
-#     return  -1
+def finder(arr1, arr2):
+    a = {}
+    for i in arr1:
+        if i not in a:
+            a[i] = 1
+        else:
+            a[i] = a[i] + 1
+  
+    for i in arr2:
+        if i not in a:
+            return i
+        else:
+            a[i] = a[i] -1
+    print(a)
+    for i in a.keys():
+        print(f'i = {i}, a[{i}]= {a[i]}')
+        if a[i] !=0:
+            return i
+    return  -1
 
 print(finder([1,2,3,4,5,6,7], [3,7,2,1,4,6]))
 print(finder([5,5,7,7], [5,7,7]))
