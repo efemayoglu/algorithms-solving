b849f5968e00cacf1154540843533c275622bad2
efemayoglu <efemayoglu@gmail.com>
2021-03-12 20:31:18 +0300

    interviewer answer added

--- string-compression.py
+++ string-compression.py
@@ -1,20 +1,49 @@
+# def compress(s):
+#     r = ""
+#     l = len(s)
+
+#     if l == 0:
+#         return ""
+#     if l == 1:
+#         return s+"1"
+
+#     last = s[0]
+#     cnt = 1 
+#     i = 1
+
+#     while i < l:
+        
+#         if s[i] == s[i-1]:
+#             cnt += 1
+#         else:
+#             r = r + s[i-1] + str(cnt)
+#             cnt = 1
+#         i += 1
+
+#     r = r + s[i-1] + str(cnt)
+
+#     return r
+
+
+
+
+
+
+
+
 def compress(s):
     index = 0
     lastChar = s[0]
     n = ""
-
     while index < len(s):  
         counter = 0
-        
+      
         while index < len(s) and s[index] == lastChar:
             counter += 1
             index += 1
-
         n += f'{lastChar}{counter}'
-
         if index < len(s):
             lastChar = s[index]
-
     return n
 
 #manuel debugging
