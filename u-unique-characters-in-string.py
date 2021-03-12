c4c2f131e50dd172a6cbbe3e630ff0b4f2528e6b
efemayoglu <efemayoglu@gmail.com>
2021-03-12 21:45:10 +0300

    fixed quickly

---
+++ u-unique-characters-in-string.py
@@ -0,0 +1,49 @@
+def uni_char(s):
+    a = {}
+
+    for i in s:
+        if i in a:
+            return False
+        else:
+            a[i] = True
+
+    return True
+
+
+
+
+
+print(uni_char('abcdefg') ==  True)
+print(uni_char('goo') ==  False)
+print(uni_char('abcdefg') ==  True)
+
+
+
+# """
+# RUN THIS CELL TO TEST YOUR CODE>
+# """
+# from nose.tools import assert_equal
+
+
+# class TestUnique(object):
+
+#     def test(self, sol):
+#         assert_equal(sol(''), True)
+#         assert_equal(sol('goo'), False)
+#         assert_equal(sol('abcdefg'), True)
+#         print 'ALL TEST CASES PASSED'
+        
+# # Run Tests
+# t = TestUnique()
+# t.test(uni_char)
+
+
+
+
+
+# Unique Characters in String
+# Problem
+# Given a string,determine if it is compreised of all unique characters. For example, the string 'abcde' has all unique characters and should return True. The string 'aabcde' contains duplicate characters and should return false.
+
+# Solution
+# Fill out your solution below:
